6db36e4fbb613644a00ad85181d2455bbd546b63
dgaye <damegaye1887@gmail.com>
2023-09-18 12:50:42 +0000

    2nd commit 'mutability.js'

--- mutability.js
+++ mutability.js
@@ -1,13 +1,7 @@
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-  };
+const clone1 = { ...person };
+const clone2 = { ...person };
   
-  const clone1 = { ...person };
-  const clone2 = { ...person };
-  
-  const samePerson = person;
+const samePerson = person;
 
-  person.age += 1;
-  person.country = 'FR';
+person.age += 1;
+person.country = 'FR';

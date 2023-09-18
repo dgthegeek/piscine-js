ba25db3a77d22ab4d5571ed6c8bb5e2238dc78cc
dgaye <damegaye1887@gmail.com>
2023-09-18 14:52:52 +0000

    1st commit 'sign.js'

--- sign.js
+++ sign.js
@@ -9,7 +9,9 @@ function sign(num) {
 }
 
 function sameSign(num1, num2) {
-    if (num1 - num2 === 0) {
+    if (num1 > 0 && num2 > 0) {
+        return true
+    }else if (num1 < 0 && num2 < 0){
         return true
     }
     return false

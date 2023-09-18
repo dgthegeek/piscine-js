d0628f630a811aefd3e2d48da5d02ed10de0bc12
dgaye <damegaye1887@gmail.com>
2023-09-18 14:26:11 +0000

    1st commit 'abs.js'

---
+++ abs.js
@@ -0,0 +1,12 @@
+function isPositive(number) {
+    return number > 0;
+  }
+  
+  function abs(number) {
+    if (number >= 0) {
+      return number;
+    } else {
+      return -number;
+    }
+  }
+  

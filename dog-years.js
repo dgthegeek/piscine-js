e95afd4c6a03dbf2b93ebc330b46a0d6b7d7d28a
dgaye <damegaye1887@gmail.com>
2023-09-18 13:33:26 +0000

    1st commit 'dog-years.js'

---
+++ dog-years.js
@@ -0,0 +1,18 @@
+function dogYears(planet, dogAgeInSeconds) {
+    const orbitalPeriodsInEarthYears = {
+      earth: 1.0,
+      mercury: 0.2408467,
+      venus: 0.61519726,
+      mars: 1.8808158,
+      jupiter: 11.862615,
+      saturn: 29.447498,
+      uranus: 84.016846,
+      neptune: 164.79132,
+    };
+    const earthYears = dogAgeInSeconds / 31557600; 
+    const dogYearsOnPlanet = earthYears / orbitalPeriodsInEarthYears[planet];
+  
+    const roundedDogYears = dogYearsOnPlanet.toFixed(2);
+    return parseFloat(roundedDogYears); 
+  }
+  

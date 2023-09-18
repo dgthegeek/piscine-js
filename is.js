656112d821519d3091edd9cc0fd586c8f6b9f8ea
dgaye <damegaye1887@gmail.com>
2023-09-18 15:33:34 +0000

    fixed

--- is.js
+++ is.js
@@ -1,12 +1,11 @@
-
 is.num = (value) => typeof value === 'number'
-is.nan= (value) => isNaN(value),
-is.str= (value) => typeof value === 'string'
-is.bool= (value) => typeof value === 'boolean'
-is.undef= (value) => value === undefined
-is.def= (value) => value !== undefined
-is.arr= (value) => Array.isArray(value)
-is.obj= (value) => value !== null && typeof value === 'object'
-is.fun= (value) => typeof value === 'function'
-is.truthy= (value) => Boolean(value)
-is.falsy= (value) => !value
+is.nan = (value) => Number.isNaN(value)
+is.str = (value) => typeof value === 'string'
+is.bool = (value) => typeof value === 'boolean'
+is.undef = (value) => value === undefined
+is.def = (value) => value !== undefined
+is.arr = (value) => Array.isArray(value)
+is.obj = (value) => value !== null && typeof value === 'object'
+is.fun = (value) => typeof value === 'function'
+is.truthy = (value) => Boolean(value)
+is.falsy = (value) => !value

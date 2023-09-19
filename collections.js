77ec6c749c5d233359090c2f734fc0373bf9b550
dgaye <damegaye1887@gmail.com>
2023-09-19 12:17:46 +0000

    trying again

--- collections.js
+++ collections.js
@@ -41,10 +41,11 @@ function arrToSet(arr) {
   
   function arrToObj(arr) {
     const obj = {};
-    arr.forEach(([key, value]) => {
-      obj[`${key}`] = `${value}`;
-    });
-    return obj;
+    for (let i = 0; i < arr.length; i++) {
+      const [key, value] = arr[i];
+      obj[key] = value;
+    }
+    return obj
   }
   
   function strToObj(str) {

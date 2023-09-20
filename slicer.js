1b548745e99334f817d8166579c86ade360bc090
dgaye <damegaye1887@gmail.com>
2023-09-20 15:03:28 +0000

    1st commit

---
+++ slicer.js
@@ -0,0 +1,21 @@
+function slice(input, startIndex, endIndex) {
+    const length = input.length;
+  
+    if (startIndex < 0) {
+      startIndex = Math.max(0, length + startIndex);
+    }
+  
+    if (endIndex === undefined) {
+      endIndex = length;
+    } else if (endIndex < 0) {
+      endIndex = Math.max(0, length + endIndex);
+    }
+  
+    if (Array.isArray(input)) {
+      return input.slice(startIndex, endIndex);
+    } else if (typeof input === 'string') {
+      return input.slice(startIndex, endIndex);
+    }
+  }
+  
+  

20036afbde0a1be445c305022db1e93dd99a336f
dgaye <damegaye1887@gmail.com>
2023-09-20 15:27:18 +0000

    4st commit

--- slicer.js
+++ slicer.js
@@ -1,18 +1,22 @@
 function slice(input, startIndex, endIndex) {
     const length = input.length;
-  
+
     if (startIndex < 0) {
-      startIndex = Math.max(0, length + startIndex);
+        startIndex = Math.max(0, length + startIndex);
     }
-  
+
     if (endIndex === undefined) {
-      endIndex = length;
+        endIndex = length;
     } else if (endIndex < 0) {
-      endIndex = Math.max(0, length + endIndex);
+        endIndex = Math.max(0, length + endIndex);
     }
-  
-    if (Array.isArray(input)) {
-      return input.slice(startIndex, endIndex);
+
+    let result = [];
+    for (let index = startIndex; index < endIndex; index++) {
+
+      result += input[index];
     }
+    return result
+
+
 }
-  

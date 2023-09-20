6d48c08d27bee3bb28cbf7f3138ffabc8ed44a93
dgaye <damegaye1887@gmail.com>
2023-09-20 19:01:21 +0000

    4st commit

--- reverse.js
+++ reverse.js
@@ -3,7 +3,13 @@ function reverse(s) {
     for (let index = s.length-1; index >= 0; index--) {
         res.push(s[index])
     }
+    if (typeof res[0] === "string") {
+     
+        return res.join("")
+    }
     return res
 }
+
+
 
 console.log(reverse([1,2,3,4]))

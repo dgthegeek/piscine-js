0b11abcbf0166e1fddbbc9ab49a1682c692acc76
dgaye <damegaye1887@gmail.com>
2023-09-20 13:08:28 +0000

    1st commit

--- elementary.js
+++ elementary.js
@@ -1,4 +1,3 @@
-// Function to mimic the * (multiply) operator
 function multiply(a, b) {
   let result = 0;
   for (let i = 0; i < b; i++) {
@@ -7,12 +6,7 @@ function multiply(a, b) {
   return result;
 }
 
-// Function to mimic the / (integer division) operator
 function divide(a, b) {
-  if (b === 0) {
-    throw new Error("Division by zero is not allowed.");
-  }
-
   let quotient = 0;
   let remaining = a;
 
@@ -24,12 +18,7 @@ function divide(a, b) {
   return quotient;
 }
 
-// Function to mimic the % (modulo) operator
 function modulo(a, b) {
-  if (b === 0) {
-    throw new Error("Division by zero is not allowed.");
-  }
-
   let remaining = a;
   while (remaining >= b) {
     remaining -= b;
@@ -37,12 +26,3 @@ function modulo(a, b) {
 
   return remaining;
 }
-
-// Example usages:
-const product = multiply(5, 3);
-const quotient = divide(15, 5);
-const remainder = modulo(15, 7);
-
-console.log("Product:", product); // Output: Product: 15
-console.log("Quotient:", quotient); // Output: Quotient: 3
-console.log("Remainder:", remainder); // Output: Remainder: 1

e691cd1cc935fd3058c8ab29fd3436e876a4f020
dgaye <damegaye1887@gmail.com>
2023-09-20 14:01:15 +0000

    1st commit

--- index-of.js
+++ index-of.js
@@ -7,18 +7,19 @@ function indexOf(arr, value, startIndex = 0) {
     return -1;
   }
   
-  function lastIndexOf(arr, value) {
-    for (let i = arr.length - 1; i >= 0; i--) {
-      if (arr[i] === value) {
+  function lastIndexOf(arr, value, start) {
+    for (let i = arr.length - start; i >= 0; i--) {
+      if (arr[i] === value.toString()) {
         return i;
       }
+      
     }
     return -1;
   }
   
-  function includes(arr, value) {
+  function includes(arr, value, start) {
     return arr.includes(value);
   }
   
 
-  
+console.log(lastIndexOf(['t', 0, 0, 't'], 't', 2))

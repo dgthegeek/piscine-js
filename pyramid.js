a0e52f2d38a9bd1cdf37b2b384ab33706e50aa6d
mabdelwe <mouhamedvadel2021@gmail.com>
2023-09-21 15:37:45 +0000

    lets give it a first try

--- pyramid.js
+++ pyramid.js
@@ -1,8 +1,9 @@
 function pyramid(char, n) {
+
     const pyramidArray = Array.from({ length: n }, (_, i) => {
-        const spaces = " ".repeat(n - i+1);
-        const blocks = char.repeat(2 * i + 1);
-        return spaces + blocks;
+        const spaces = " ".repeat(n - i-1);
+        const str = char.repeat(2 * i + 1);
+        return spaces + str;
     });
 
     return pyramidArray.join('\n');

8414c0b4d8a0e785814fc21718b92848fc33d96d
mabdelwe <mouhamedvadel2021@gmail.com>
2023-09-21 13:15:33 +0000

    lets give it a first try

---
+++ molecules-cells.js
@@ -0,0 +1,51 @@
+function transcribeToRNA(dna) {
+    const dna = dna.split("");
+    const rna = [];
+
+    for (let i = 0; i < dna.length; i++) {
+        switch (dna[i]) {
+            case "G":
+                rna.push("C");
+                break;
+            case "C":
+                rna.push("G");
+                break;
+            case "T":
+                rna.push("A");
+                break;
+            case "A":
+                rna.push("U");
+                break;
+            default:
+                break;
+        }
+    }
+
+    return rna.join("");
+}
+
+function transcribeToDNA(rna) {
+    const rna = rna.split("");
+    const dna = [];
+
+    for (let i = 0; i < rna.length; i++) {
+        switch (rna[i]) {
+            case "C":
+                dna.push("G");
+                break;
+            case "G":
+                dna.push("C");
+                break;
+            case "A":
+                dna.push("T");
+                break;
+            case "U":
+                dna.push("A");
+                break;
+            default:
+                break;
+        }
+    }
+
+    return dna.join("");
+}

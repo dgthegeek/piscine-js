c91332c15eec3ceab4271366d3e9cefc219bbdb7
dgaye <damegaye1887@gmail.com>
2023-09-22 15:35:58 +0000

    first try

--- same-amount.js
+++ same-amount.js
@@ -3,8 +3,9 @@ function sameAmount(s, reg1, reg2) {
     const res1 = s.match(reg1);
     const res2 = s.match(reg2);
 
-    if (res1.length === res1.length ) {
+    if (res1.length === res2.length ) {
         return true
     }
     return false
 }
+console.log(sameAmount('hello how are you', /l/, /e/))

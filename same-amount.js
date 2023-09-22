61c0dff0e141e5a3aa01867f0f6d016a8366fe2e
dgaye <damegaye1887@gmail.com>
2023-09-22 15:34:24 +0000

    first try

---
+++ same-amount.js
@@ -0,0 +1,10 @@
+
+function sameAmount(s, reg1, reg2) {
+    const res1 = s.match(reg1);
+    const res2 = s.match(reg2);
+
+    if (res1.length === res1.length ) {
+        return true
+    }
+    return false
+}

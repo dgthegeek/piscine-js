8c16befec69175501dcb3270a62de0e8740e95c7
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2023-09-24 01:22:56 +0000

    Update 'greedy-url.js'

--- greedy-url.js
+++ greedy-url.js
@@ -1,22 +1,21 @@
 const urlPattern = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}(\.[a-zA-Z0-9()]{1,6})?(?:[-a-zA-Z0-9()\[\],@:%_\+.~#?&\/=]*)/g;
 
-function getURL(data) {
-    const urls = data.match(urlPattern);
-    return urls;
+function getURL(dataSet) {
+  return dataSet.match(urlPattern) || [];
 }
 
-function greedyQuery(urls) {
-    const filteredUrls = urls.filter(url => {
-        const queryParameters = url.match(/\?([-a-zA-Z0-9\[\],()@]*=[-a-zA-Z0-9\[\],()@]*&){2,255}([-a-zA-Z0-9\[\],()@]*=[-a-zA-Z0-9\[\],()@]*)/g);
-        return queryParameters !== null;
-    });
-    return filteredUrls;
+function greedyQuery(dataSet) {
+  const urls = extractURLs(dataSet);
+  return urls.filter((url) => {
+    const queryPattern = /\?([-a-zA-Z0-9\[\],()@]*=[-a-zA-Z0-9\[\],()@]*&){2,255}([-a-zA-Z0-9\[\],()@]*=[-a-zA-Z0-9\[\],()@]*)/g;
+    return queryPattern.test(url);
+  });
 }
 
-function notSoGreedy(urls) {
-    const filteredUrls = urls.filter(url => {
-        const queryParameters = url.match(/\?([-a-zA-Z0-9\[\],()@%]*=[-a-zA-Z0-9\[\],()@%]*&){1,2}([-a-zA-Z0-9\[\],()@%]*=[-a-zA-Z0-9\[\],()@%]*)$/g);
-        return queryParameters !== null;
-    });
-    return filteredUrls;
+function notSoGreedys(dataSet) {
+  const urls = extractURLs(dataSet);
+  return urls.filter((url) => {
+    const queryPattern = /\?([-a-zA-Z0-9\[\],()@%]*=[-a-zA-Z0-9\[\],()@%]*&){1,2}([-a-zA-Z0-9\[\],()@%]*=[-a-zA-Z0-9\[\],()@%]*)$/g;
+    return queryPattern.test(url);
+  });
 }

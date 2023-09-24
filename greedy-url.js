498d1b1d30ce133806eed247f2981b95a23753fc
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2023-09-24 01:26:04 +0000

    Update 'greedy-url.js'

--- greedy-url.js
+++ greedy-url.js
@@ -5,7 +5,7 @@ function getURL(dataSet) {
 }
 
 function greedyQuery(dataSet) {
-  const urls = extractURLs(dataSet);
+  const urls = getURL(dataSet);
   return urls.filter((url) => {
     const queryPattern = /\?([-a-zA-Z0-9\[\],()@]*=[-a-zA-Z0-9\[\],()@]*&){2,255}([-a-zA-Z0-9\[\],()@]*=[-a-zA-Z0-9\[\],()@]*)/g;
     return queryPattern.test(url);
@@ -13,7 +13,7 @@ function greedyQuery(dataSet) {
 }
 
 function notSoGreedys(dataSet) {
-  const urls = extractURLs(dataSet);
+  const urls = getURL(dataSet);
   return urls.filter((url) => {
     const queryPattern = /\?([-a-zA-Z0-9\[\],()@%]*=[-a-zA-Z0-9\[\],()@%]*&){1,2}([-a-zA-Z0-9\[\],()@%]*=[-a-zA-Z0-9\[\],()@%]*)$/g;
     return queryPattern.test(url);

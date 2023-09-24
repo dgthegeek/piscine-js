00fd8e6ecfac2b4b5af46ca0cef37239b03d4871
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2023-09-24 01:41:18 +0000

    Update 'valid-ip.js'

--- valid-ip.js
+++ valid-ip.js
@@ -1,6 +1,4 @@
 function findIP(str) {
-  const ipPattern =
-    /\b(?:\d{1,3}\.){3}\d{1,3}\b/g;
-
-  return str.match(ipPattern) || [];
+  return str.match(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/g);
 }
+

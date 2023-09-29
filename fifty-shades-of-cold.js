49b9eaeacd39916a2542bfa1990bcad106b4047e
dgaye <damegaye1887@gmail.com>
2023-09-29 15:56:33 +0000

    dd

---
+++ fifty-shades-of-cold.js
@@ -0,0 +1,26 @@
+import { colors } from "./fifty-shades-of-cold.data.js";
+
+export function generateStyles() {
+    const head = document.getElementsByTagName("head")[0];
+    const style = document.createElement("style");
+    style.innerHTML = colors.map(color => `.${color} { background: ${color}; }\n`).join('');
+    head.appendChild(style);
+}
+
+export function generateColdShades() {
+    const body = document.getElementsByTagName("body")[0];
+    colors.filter(color => /(aqua|blue|turquoise|green|cyan|navy|purple)/.test(color))
+          .forEach(color => {
+              const div = document.createElement("div");
+              div.classList.add(color);
+              div.textContent = color;
+              body.appendChild(div);
+          });
+}
+
+export function chooseShade(shade) {
+    document.querySelectorAll("div").forEach(div => {
+        div.className = shade;
+    });
+}
+

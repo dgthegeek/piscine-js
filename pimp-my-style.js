9e95299a6d9254e0259717d010afef4909c81aba
dgaye <damegaye1887@gmail.com>
2023-09-29 15:01:16 +0000

    dd

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,25 +1,25 @@
 import { styles } from './pimp-my-style.data.js';
 
-let counter = 0; 
+let counter = 0;
 
 export function pimp() {
     const button = document.querySelector("button");
     const isUnpimped = button.classList.contains("unpimp");
-    
-    button.classList.add(styles[counter]);
-    counter++;
-
-    if (counter === styles.length-1) {
-        button.classList.add("unpimp"); 
-    }
-
     if (isUnpimped) {
-        button.classList.remove("unpimp");
-        button.classList.remove(styles[counter]); 
         counter--;
+        button.classList.remove(styles[counter]);
 
         if (counter <= 0) {
-            counter = 0; 
+            counter = 0;
+            button.classList.remove("unpimp");
+
         }
-    } 
+    } else {
+        button.classList.add(styles[counter]);
+        counter++;
+        if (counter === styles.length - 1) {
+            button.classList.add("unpimp");
+        }
+    }
+
 }

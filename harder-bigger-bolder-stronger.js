c89008c1b0e3b03f6cfb67263fd8b5e45706f124
dgaye <damegaye1887@gmail.com>
2023-09-29 00:15:41 +0000

    dd

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,22 @@
+export function generateletterDivs() {
+    const body = document.body;
+
+    for (let i = 0; i < 120; i++) {
+        const letterDiv = document.createElement("div")
+        const fontSize = 11 + i
+        letterDiv.style.fontSize = `${fontSize}px`
+        const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        letterDiv.textContent = letters[Math.floor(Math.random() * letters.length)]
+
+
+        if (i < 40) {
+            letterDiv.style.fontWeight = "300"
+        } else if (i < 80) {
+            letterDiv.style.fontWeight = "400"
+        } else {
+            letterDiv.style.fontWeight = "600"
+        }
+
+        body.appendChild(letterDiv)
+    }
+}

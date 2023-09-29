f30311e34b680bb11930e6f5f58fcc2ff75aad24
dgaye <damegaye1887@gmail.com>
2023-09-29 16:38:09 +0000

    dd

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -1,28 +1,26 @@
 document.addEventListener("keydown", compose);
 
-function compose(event) {
+export function compose(event) {
     if (!event) return;
 
     const key = event.key;
-    const notes = document.getElementsByClassName("note");
 
     if (/[a-z]/.test(key)) {
-        const colorValue = (255 / 26) * (key.charCodeAt(0) - 97);
-        const rgbColor = `rgb(${colorValue}, ${colorValue}, ${colorValue})`;
-
         const div = document.createElement("div");
         div.classList.add("note");
-        div.style.backgroundColor = rgbColor;
+        div.style.backgroundColor = `rgb(${(255 / 26) * (key.charCodeAt(0) - 97)}, ${(255 / 26) * (key.charCodeAt(0) - 97)}, ${(255 / 26) * (key.charCodeAt(0) - 97)})`;
         div.textContent = key;
-
         document.body.appendChild(div);
-    } else if (key === "Backspace" && notes.length > 0) {
-        notes[notes.length - 1].remove();
+    } else if (key === "Backspace") {
+        let notes = document.getElementsByClassName("note");
+        if (notes.length > 0) {
+            notes[notes.length - 1].remove();
+        }
     } else if (key === "Escape") {
+        let notes = document.getElementsByClassName("note");
         while (notes.length > 0) {
             notes[0].remove();
         }
     }
 }
 
-export { compose };

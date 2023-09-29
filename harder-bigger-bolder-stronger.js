14a13c096f6e8e37295e7f3bdbc5ea91f52ca539
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2023-09-29 01:27:56 +0000

    Update 'harder-bigger-bolder-stronger.js'

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -1,22 +1,22 @@
 export function generateletterDivs() {
     const body = document.body;
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
     for (let i = 0; i < 120; i++) {
-        const letterDiv = document.createElement("div")
-        const fontSize = 11 + i
-        letterDiv.style.fontSize = `${fontSize}px`
-        const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        letterDiv.textContent = letters[Math.floor(Math.random() * letters.length)]
-
-
-        if (i < 40) {
-            letterDiv.style.fontWeight = "300"
-        } else if (i < 80) {
-            letterDiv.style.fontWeight = "400"
-        } else {
-            letterDiv.style.fontWeight = "600"
-        }
-
-        body.appendChild(letterDiv)
+        const letterDiv = createLetterDiv(11 + i, letters);
+        body.appendChild(letterDiv);
     }
+}
+
+function createLetterDiv(fontSize, letters) {
+    const letterDiv = document.createElement("div");
+    letterDiv.style.fontSize = `${fontSize}px`;
+
+    const randomLetterIndex = Math.floor(Math.random() * letters.length);
+    letterDiv.textContent = letters[randomLetterIndex];
+
+    const fontWeight = i < 40 ? "300" : (i < 80 ? "400" : "600");
+    letterDiv.style.fontWeight = fontWeight;
+
+    return letterDiv;
 }

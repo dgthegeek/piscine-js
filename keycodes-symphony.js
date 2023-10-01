75f811b46593b5c814911cb076864b0071771990
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2023-10-01 00:20:30 +0000

    Update 'keycodes-symphony.js'

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -1,26 +1,34 @@
-document.addEventListener("keydown", compose);
+document.addEventListener("keydown", function (event) {
+    compose(event);
+});
 
-export function compose(event) {
-    if (!event) return;
-
-    const key = event.key;
-
-    if (/[a-z]/.test(key)) {
-        const div = document.createElement("div");
-        div.classList.add("note");
-        div.style.backgroundColor = `rgb(${(255 / 26) * (key.charCodeAt(0) - 97)}, ${(255 / 26) * (key.charCodeAt(0) - 97)}, ${(255 / 26) * (key.charCodeAt(0) - 97)})`;
-        div.textContent = key;
-        document.body.appendChild(div);
-    } else if (key === "Backspace") {
+export function compose(e) {
+    if (e === undefined) {
+        return;
+    }
+    if (
+        [...Array(26).keys()].map((i) => i + 97).includes(e.key.charCodeAt(0))
+    ) {
+        let noteDiv = document.createElement("div");
+        noteDiv.classList.add("note");
+        noteDiv.style.backgroundColor = `rgb(${
+            (255 / 26) * (e.key.charCodeAt(0) - 97)
+        }, ${(255 / 26) * (e.key.charCodeAt(0) - 97)}, ${
+            (255 / 26) * (e.key.charCodeAt(0) - 97)
+        })`;
+        noteDiv.innerHTML = e.key;
+        document.body.appendChild(noteDiv);
+    } else if (e.key === "Backspace") {
         let notes = document.getElementsByClassName("note");
         if (notes.length > 0) {
             notes[notes.length - 1].remove();
         }
-    } else if (key === "Escape") {
+    } else if (e.key === "Escape") {
         let notes = document.getElementsByClassName("note");
         while (notes.length > 0) {
             notes[0].remove();
         }
     }
 }
+
 

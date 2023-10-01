2625dcd9e78a2d49bad2329db67b373a88656d12
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2023-10-01 00:30:08 +0000

    Update 'mouse-trap.js'

--- mouse-trap.js
+++ mouse-trap.js
@@ -7,46 +7,61 @@ class Circle {
         this.y = y;
         this.diameter = 50;
         this.isTrapped = false;
+        this.HTML = null;
+        this.draw();
+        circles.push(this);
+    }
+
+    draw() {
         this.createCircleElement();
-        this.addToCircles();
+        this.updatePosition();
+        this.trapped();
     }
 
     createCircleElement() {
-        this.element = document.createElement("div");
-        this.element.classList.add("circle");
-        this.element.style.cssText = `position: absolute; top: ${this.y}px; left: ${this.x}px; background: white;`;
-        this.updateTrappedState();
-        document.body.appendChild(this.element);
+        this.HTML = document.createElement("div");
+        this.HTML.classList.add("circle");
+        this.HTML.style.cssText = `
+            position: absolute;
+            background: white;
+        `;
+        document.body.appendChild(this.HTML);
+    }
+
+    updatePosition() {
+        this.HTML.style.top = this.y + "px";
+        this.HTML.style.left = this.x + "px";
     }
 
     move(x, y) {
-        this.updateTrappedState();
+        this.trapped();
         if (!this.isTrapped) {
             this.x = x;
             this.y = y;
-            this.element.style.cssText = `position: absolute; top: ${this.y}px; left: ${this.x}px;`;
+            this.updatePosition();
         } else {
             if (this.isInsideBox(x, y)) {
                 this.x = x;
                 this.y = y;
-                this.element.style.cssText = `position: absolute; top: ${this.y}px; left: ${this.x}px;`;
+                this.updatePosition();
             } else {
                 if (this.isInsideBox(x, this.y)) {
                     this.x = x;
-                    this.element.style.cssText = `position: absolute; left: ${this.x}px;`;
+                    this.updatePosition();
                 } else if (this.isInsideBox(this.x, y)) {
                     this.y = y;
-                    this.element.style.cssText = `position: absolute; top: ${this.y}px;`;
+                    this.updatePosition();
                 }
             }
         }
     }
 
-    updateTrappedState() {
+    trapped() {
         const { x, y, diameter } = this;
-        const isInsideBox = x > box.x && x + diameter < box.x + box.width && y > box.y && y + diameter < box.y + box.height;
+        const { x: bx, y: by, width, height } = box;
+        const isInsideBox = x > bx && x + diameter < bx + width && y > by && y + diameter < by + height;
         this.isTrapped = isInsideBox;
-        this.element.style.background = isInsideBox ? "var(--purple)" : "white";
+        this.HTML.style.background = isInsideBox ? "var(--purple)" : "white";
     }
 
     isInsideBox(x, y) {
@@ -62,14 +77,19 @@ class Box {
     }
 
     createBoxElement() {
-        this.element = document.createElement("div");
-        this.element.classList.add("box");
-        this.element.style.cssText = "position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);";
-        document.body.appendChild(this.element);
+        this.HTML = document.createElement("div");
+        this.HTML.classList.add("box");
+        this.HTML.style.cssText = `
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        `;
+        document.body.appendChild(this.HTML);
     }
 
     calculateDimensions() {
-        const { offsetLeft, offsetTop, offsetWidth, offsetHeight } = this.element;
+        const { offsetLeft, offsetTop, offsetWidth, offsetHeight } = this.HTML;
         this.x = offsetLeft - offsetWidth / 2 - 1;
         this.y = offsetTop - offsetHeight / 2 - 1;
         this.width = offsetWidth + 1;
@@ -77,16 +97,15 @@ class Box {
     }
 }
 
-document.body.addEventListener("click", (e) => createCircleHandler(e));
-document.body.addEventListener("mousemove", (e) => moveCircleHandler(e));
+document.body.addEventListener("click", createCircle);
+document.body.addEventListener("mousemove", moveCircle);
 
-function createCircleHandler(e) {
+function createCircle(e) {
     if (e === undefined) return;
-    const circle = new Circle(e.clientX - 25, e.clientY - 25);
-    circles.push(circle);
+    new Circle(e.clientX - 25, e.clientY - 25);
 }
 
-function moveCircleHandler(e) {
+function moveCircle(e) {
     if (e === undefined || circles.length === 0) return;
     circles[circles.length - 1].move(e.clientX - 25, e.clientY - 25);
 }
@@ -95,4 +114,4 @@ function setBox() {
     box = new Box();
 }
 
-export { createCircleHandler as createCircle, moveCircleHandler as moveCircle, setBox };
+export { createCircle, moveCircle, setBox };

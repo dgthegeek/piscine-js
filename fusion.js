fb580a363bb78e47e1bb1a37eb655d63db4e8fe2
dgaye <damegaye1887@gmail.com>
2023-10-02 18:09:08 +0000

    d

--- fusion.js
+++ fusion.js
@@ -1,38 +1,38 @@
-function fusion(objA, objB) {
-    var merged = {};
+function fusion(obj1, obj2) {
+    const fusioned = {};
 
-    for (var keyA in objA) {
-        if (!objA.hasOwnProperty(keyA)) continue;
-        if (objB.hasOwnProperty(keyA)) {
-            if (isObject(objA[keyA]) && isObject(objB[keyA])) {
-                merged[keyA] = fusion(objA[keyA], objB[keyA]);
-            } else if (isArray(objA[keyA]) && isArray(objB[keyA])) {
-                merged[keyA] = objA[keyA].concat(objB[keyA]);
-            } else if (isNumber(objA[keyA]) && isNumber(objB[keyA])) {
-                merged[keyA] = objA[keyA] + objB[keyA];
-            } else if (isString(objA[keyA]) && isString(objB[keyA])) {
-                merged[keyA] = objA[keyA] + " " + objB[keyA];
+    const is = {};
+    is.num = (n) => typeof n === "number";
+    is.str = (n) => typeof n === "string";
+    is.arr = (n) => Array.isArray(n);
+    is.obj = (n) => typeof n === "object" && !is.fun(n) && !is.arr(n) && n !== null;
+    is.fun = (n) => typeof n === "function";
+
+    for (const key in obj1) {
+        if (obj1.hasOwnProperty(key)) {
+            if (obj2.hasOwnProperty(key)) {
+                if (is.obj(obj1[key]) && is.obj(obj2[key])) {
+                    fusioned[key] = fusion(obj1[key], obj2[key]);
+                } else if (is.arr(obj1[key]) && is.arr(obj2[key])) {
+                    fusioned[key] = obj1[key].concat(obj2[key]);
+                } else if (is.num(obj1[key]) && is.num(obj2[key])) {
+                    fusioned[key] = obj1[key] + obj2[key];
+                } else if (is.str(obj1[key]) && is.str(obj2[key])) {
+                    fusioned[key] = obj1[key] + " " + obj2[key];
+                } else {
+                    fusioned[key] = obj2[key];
+                }
             } else {
-                merged[keyA] = objB[keyA];
+                fusioned[key] = obj1[key];
             }
-        } else {
-            merged[keyA] = objA[keyA];
         }
     }
 
-    for (var keyB in objB) {
-        if (!objB.hasOwnProperty(keyB)) continue;
-        if (!objA.hasOwnProperty(keyB)) {
-            merged[keyB] = objB[keyB];
+    for (const key in obj2) {
+        if (obj2.hasOwnProperty(key) && !obj1.hasOwnProperty(key)) {
+            fusioned[key] = obj2[key];
         }
     }
 
-    return merged;
+    return fusioned;
 }
-
-const is = {};
-is.isNumber = (n) => typeof n === "number";
-is.isString = (n) => typeof n === "string";
-is.isArray = (n) => Array.isArray(n);
-is.isObject = (n) => typeof n === "object" && !isFunction(n) && !isArray(n) && n !== null;
-is.isFunction = (n) => typeof n === "function";

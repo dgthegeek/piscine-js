e92848138142f2f3c8f6b78bdc4cde8867eb646d
dgaye <damegaye1887@gmail.com>
2023-10-02 14:29:33 +0000

    d

--- pick-omit.js
+++ pick-omit.js
@@ -1,15 +1,20 @@
 function pick(obj, s) {
-    let result = {};
-    const keys = Object.keys(obj)
-    s.forEach (el => {
-        for (let i = 0; i < keys.length; i++) {
-            if (el === keys[i]){
-                result[keys[i]] = obj[keys[i]]
-            }      
+    const result = {}
+    let keys = Object.keys(obj)
+    keys.forEach(key => {
+        if (s.includes(key)) {
+            result[key] = obj[key]
         }
-    });
+    })
     return result
-    
 }
-
-console.log(pick({ something: 5, __proto__: { d: 6 } }, ['proto', 'something']));
+function omit(obj, s) {
+    const result = {}
+    let keys = Object.keys(obj)
+    keys.forEach(key => {
+        if (!s.includes(key)) {
+            result[key] = obj[key]
+        }
+    })
+    return result
+}

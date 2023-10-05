04f4da7efd1fadfeaf9b9e8e0207714c4f5d4e16
dgaye <damegaye1887@gmail.com>
2023-10-05 01:05:39 +0000

    fixed

--- flagger.js
+++ flagger.js
@@ -1,13 +1,22 @@
 function flags(obj) {
     const res = { alias: { h: 'help' } };
-  
-    if (Object.keys(obj).length === 0) return res;
-  
+
+    if (Object.keys(obj).length === 0) {
+        return res;
+    }
+
     const help = obj.help;
     delete obj.help;
-  
-    res.description = (help || Object.keys(obj)).map(key => `-${key[0]}, --${key}: ${obj[key]}`).join('\n');
-  
+
+    for (const key in obj) {
+        res.alias[key[0]] = key;
+    }
+
+    const descriptions = help
+        ? Object.keys(obj).map((key) => `-${key[0]}, --${key}: ${obj[key]}`)
+        : Object.entries(obj).map(([key, desc]) => `-${key[0]}, --${key}: ${desc}`);
+
+    res.description = descriptions.length === 1 ? descriptions[0] : descriptions.join('\n');
+
     return res;
-  }
-  
+}

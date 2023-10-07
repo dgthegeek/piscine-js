c486719a2ca0e2f1fb57ab27c8340b4e42db6fef
dgaye <damegaye1887@gmail.com>
2023-10-07 01:19:28 +0000

    fixed

--- throttle.js
+++ throttle.js
@@ -1,50 +1,42 @@
-function opThrottle(fn, delay, { leading = false, trailing = true } = {}) {
-    let last = 0;
-    let timer = null;
-
-    const throttled = function () {
-        const now = Date.now();
-
-        if (!last && !leading) {
-            last = now;
-        }
-
-        if (now - last >= delay) {
-            if (timer) {
-                clearTimeout(timer);
-                timer = null;
-            }
-            fn.apply(this, arguments);
-            last = now;
-        } else if (!timer && trailing) {
-            timer = setTimeout(() => {
-                fn.apply(this, arguments);
-                last = Date.now();
-                timer = null;
-            }, delay - (now - last));
-        }
-    };
-
-    throttled.cancel = () => {
-        if (timer) {
-            clearTimeout(timer);
-            timer = null;
-        }
-    };
-
-    return throttled;
-}
 function throttle(fn, delay) {
     let last = 0;
     let timer = null;
 
     const throttled = function () {
-        const now = Date.now();
-        if (now - last >= delay) {
+        const now = +new Date();
+        if (now - last > delay) {
             fn.apply(this, arguments);
             last = now;
         } else {
-            clearTimeout(timer);
+            if (timer) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(() => {
+                fn.apply(this, arguments);
+                last = now;
+            }, delay - (now - last));
+        }
+    };
+
+    return throttled;
+}
+
+function opThrottle(fn, delay, { leading = false, trailing = true } = {}) {
+    let last = 0;
+    let timer = null;
+
+    const throttled = function () {
+        const now = +new Date();
+        if (!last && !leading) {
+            last = now;
+        }
+        if (now - last > delay) {
+            if (timer) {
+                clearTimeout(timer);
+            }
+            fn.apply(this, arguments);
+            last = now;
+        } else if (!timer && trailing) {
             timer = setTimeout(() => {
                 fn.apply(this, arguments);
                 last = now;

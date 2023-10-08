a2fedcbdc9d78d4cb9cdc6f930a1401134c23820
dgaye <damegaye1887@gmail.com>
2023-10-08 02:30:40 +0000

    fixed

---
+++ all.js
@@ -0,0 +1,13 @@
+async function awaitAll(promisesMap = {}) {
+    const results = {};
+
+    if (Object.keys(promisesMap).length === 0) {
+        return {};
+    }
+
+    for (let key in promisesMap) {
+        results[key] = await promisesMap[key];
+    }
+
+    return results;
+}
